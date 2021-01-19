0e7121f3c75b383bea22dc2d570480d9a451049e
ranazohaib1 <76841808+ranazohaib1@users.noreply.github.com>
2021-01-20 00:08:57 +0500

    Update app_builder.php
    there was greater than 80 characters in lines

--- application/views/app/app_builder.php
+++ application/views/app/app_builder.php
@@ -2,7 +2,8 @@
 <?php
 if ($view_list) {
     ?>
-    <form style='margin: auto;width: 980px;' action="<?= base_url() ?>app-landing-page/<?php echo $app_id; ?>" method="POST" class="full validate add_task_form"  id='app_view'/>
+    <form style='margin: auto;width: 980px;' action="<?= base_url() ?>
+     app-landing-page/<?php echo $app_id; ?>" method="POST" class="full validate add_task_form"  id='app_view'/>
     <select name='view_id' id='view_id' />
     <option value="default">Default View</option>
     <?php
@@ -21,15 +22,20 @@ if ($view_list) {
     <?php
 }
 ?>
-<form style='margin: auto;width: 980px; position: relative;' action="<?= base_url() ?>app/edit/<?php echo $app_id; ?>" method="POST" class="full validate add_task_form"  enctype="multipart/form-data" id='app_edit'/>
+<form style='margin: auto;width: 980px; position: relative;' action="<?= base_url() ?>
+ app/edit/<?php echo $app_id; ?>" method="POST" class="full validate add_task_form"  
+      enctype="multipart/form-data" id='app_edit'/>
 <input type="hidden" id="is_edit" name="is_edit" value="0">
-<table cellpadding="0" cellspacing="0" border="0" class="display" id="example" style='width: 969px;box-shadow:0 0 6px rgba(0, 0, 0, 0.25);margin:15px 0 14px 0'>
+<table cellpadding="0" cellspacing="0" border="0" class="display" 
+       id="example" style='width: 969px;box-shadow:0 0 6px rgba(0, 0, 0, 0.25);margin:15px 0 14px 0'>
     <tbody>
         <tr>
             <td style='width: 123px;'><strong>Application Name</strong></td>
-            <td  style='width: 150px;'><input class="required textBoxLogin" type="text" name="app_name" id="d1_textfield" value="<?php echo $name; ?>" /></td>
+            <td  style='width: 150px;'><input class="required textBoxLogin" type="text"
+                                              name="app_name" id="d1_textfield" value="<?php echo $name; ?>" /></td>
             <td  style='width: 235px;'>
-                <strong style="width: 110px; line-height: 20px; margin-top: 8px;">Application Icon (.png only)</strong>
+                <strong style="width: 110px; line-height: 20px; margin-top:
+                               8px;">Application Icon (.png only)</strong>
 
                 <img id='img_app_icon' width="50" height="50" src="<?php
 //                $file_headers = @get_headers(FORM_IMG_DISPLAY_PATH . '../form_icons/' . $app_id . '/' . $icon);
@@ -37,7 +43,9 @@ if ($view_list) {
 //                echo "<pre>";
 //                print_r($file_headers);die;
 //                if ($file_headers[0] == 'HTTP/1.1 200 OK') {
-                if(file_exists(FCPATH.'assets'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'form_icons'.DIRECTORY_SEPARATOR.$app_id.DIRECTORY_SEPARATOR.$icon)){
+                if(file_exists
+                   (FCPATH.'assets'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'data'.
+                    DIRECTORY_SEPARATOR.'form_icons'.DIRECTORY_SEPARATOR.$app_id.DIRECTORY_SEPARATOR.$icon)){
 //                    echo $imagepathicon = FORM_IMG_DISPLAY_PATH . '../form_icons/' . $app_id . '/' . $icon;
                     echo $imagepathappicon = base_url() . 'assets/images/data/form_icons/' . $app_id . '/' . $icon;
                 } else {
@@ -45,19 +53,23 @@ if ($view_list) {
                     $icon='default_app.png';
                 }
                 ?>" alt="" onClick="$('#userfile').click();"/>
-                <input type="file" name="userfile" id="userfile" accept="*.png" onchange="check_file()" style='display:none;'/>
+                <input type="file" name="userfile" id="userfile"
+                       accept="*.png" onchange="check_file()" style='display:none;'/>
 
             </td>
             <td  style='width: 235px;'>
                 <strong style="width: 110px; line-height: 20px; margin-top: 8px;">Splash Screen (.png only)</strong>
                 <img id='img_splash_screen' width="50" height="50" src="<?php
-                if(file_exists(FCPATH.'assets'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'form_icons'.DIRECTORY_SEPARATOR.$app_id.DIRECTORY_SEPARATOR.$splash_icon)){
+                if(file_exists(FCPATH.'assets'.DIRECTORY_SEPARATOR.
+                               'images'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.
+                               'form_icons'.DIRECTORY_SEPARATOR.$app_id.DIRECTORY_SEPARATOR.$splash_icon)){
                     echo $imagepathicon = base_url() . 'assets/images/data/form_icons/' . $app_id . '/' .$splash_icon;
                 } else {
                     echo $imagepathicon = FORM_IMG_DISPLAY_PATH . '../form_icons/splash.png';
                 }
                 ?>" alt="" onClick="$('#splashfile').click();" style="margin-top: 7px;"/>
-                <input type="file" name="splashfile" id="splashfile" accept="*.png" onchange="check_file_splash(this)" style='display:none;'/>
+                <input type="file" name="splashfile" id="splashfile" accept="*.png"
+                       onchange="check_file_splash(this)" style='display:none;'/>
 
             </td>
             <td></td>
@@ -69,7 +81,8 @@ if ($view_list) {
             <td>
                 <a href="#" class="saveForm" id="saveForm" style="top:-10px;">Save</a>
                 <?php if($this->acl->hasPermission('app', 'build')) {?>
-                <a id="create_application" onclick="$('#is_edit').val('1');" style="top:-10px;" >Build New Version</a>
+                <a id="create_application" onclick="$('#is_edit').val('1');
+                      " style="top:-10px;" >Build New Version</a>
                 <?php }?>
             </td>
         </tr>
@@ -77,10 +90,15 @@ if ($view_list) {
         <?php if($super_app_user=='yes'){?>
         <tr>
             <td colspan="2" style="padding: 5px 0 5px 35px">
-                <a href="<?= base_url() ?>assign-applicatioin-users/<?php echo $app_id?>" style="background-color: green; padding: 5px; color: white; border-radius: 5px;text-decoration: none;"  class="cbp-tm-icon-archive">
+                <a href="<?= base_url() ?>assign-applicatioin-users/<?php echo $app_id?>"
+                   style="background-color: green; padding: 5px; color: white; 
+                          border-radius: 5px;text-decoration: none;"  class="cbp-tm-icon-archive">
                     Assign application to other users
                 </a>
-                <a target="new" href="<?= base_url() ?>web/test/<?php echo $app_id?>" style="background-color: green; padding: 5px; color: white; border-radius: 5px; margin-left: 6px;text-decoration: none;" class="cbp-tm-icon-archive">
+                <a target="new" href="<?= base_url() ?>web/test/<?php echo $app_id?>"
+                   style="background-color: green; padding: 5px; color: white; 
+                          border-radius: 5px; margin-left: 6px;text-decoration: none;
+                          " class="cbp-tm-icon-archive">
                     Test in Web Mode
                 </a>
             </td>
@@ -93,30 +111,40 @@ if ($view_list) {
 <div id="content" class="wrapper">
     <div id="controls-left">
         <ul id="add-form-title" style="display:none;">
-            <li id="title-field" rel="<?php echo $name; ?>" icon_web="<?php echo $imagepathappicon; ?>" icon_device="<?php echo $icon; ?>">Form Title</li>
+            <li id="title-field" rel="<?php echo $name; ?>"
+                icon_web="<?php echo $imagepathappicon; ?>"
+                icon_device="<?php echo $icon; ?>">Form Title</li>
         </ul>
         <ul id="form-fields">
 
-            <li style='background: none;font-size: 16px;font-weight: bold;cursor: default;'>Control Menu</li>
+            <li style='background: none;font-size: 16px;font-weight:
+                       bold;cursor: default;'>Control Menu</li>
             <?php
             if(count($forms)==0)
             {
                if($this->acl->hasPermission('form', 'add')) {?>
         
-                <a style="text-decoration:none;"><li id="add_more_form_left" style="height: 30px;"><span></span>Add Form</li></a>
+                <a style="text-decoration:none;">
+                    <li id="add_more_form_left" style="height: 30px;">
+                        <span></span>Add Form</li></a>
         
             <?php }
         
             }
             foreach ($forms as $form) {
                 ?>
-            <li id="form-icon-field" icon="<?php echo $form['icon']; ?>" icon-url="<?php echo FORM_IMG_DISPLAY_PATH . '../form_icons/' . $app_id . '/' . $form['icon']; ?>" form-url="<?php echo $form['linkfile']; ?>" form-title="<?php echo $form['title']; ?>" style="height:36px;">
+            <li id="form-icon-field" icon="<?php echo $form['icon']; ?>"
+                icon-url="<?php echo FORM_IMG_DISPLAY_PATH . '../form_icons/' .
+                    $app_id . '/' . $form['icon']; ?>" form-url="<?php echo $form['linkfile']; ?>"
+                form-title="<?php echo $form['title']; ?>" style="height:36px;">
 
 
                     <?php
                     if (!empty($form['icon'])) {
                         ?>
-                        <img alt="" title="<?php echo $form['title']; ?>" class="formIconsUpload" src="<?php echo FORM_IMG_DISPLAY_PATH . '../form_icons/' . $app_id . '/' . $form['icon'] ?>">
+                        <img alt="" title="<?php echo $form['title']; ?>"
+                             class="formIconsUpload" src="<?php echo FORM_IMG_DISPLAY_PATH .
+                            '../form_icons/' . $app_id . '/' . $form['icon'] ?>">
                         <?php
                     }?>
                         <span title="<?php echo $form['title'];?>">
@@ -148,22 +176,26 @@ if ($view_list) {
             <select id="screen_size">
                 <option value="1" <?php
 
-                if (isset($app_settings->screen_view) && $app_settings->screen_view == '1') {
+                if (isset($app_settings->screen_view) && 
+                    $app_settings->screen_view == '1') {
                     echo 'selected';
                 }
                 ?>>Development Mode</option>
                 <option value="2" <?php
-                if (isset($app_settings->screen_view) && $app_settings->screen_view == '2') {
+                if (isset($app_settings->screen_view) &&
+                    $app_settings->screen_view == '2') {
                     echo 'selected';
                 }
                 ?>>Sony Xperia Z</option>
                 <option value="3" <?php
-                if (isset($app_settings->screen_view) && $app_settings->screen_view == '3') {
+                if (isset($app_settings->screen_view) &&
+                    $app_settings->screen_view == '3') {
                     echo 'selected';
                 }
                 ?>>HTC One</option>
                 <option value="4" <?php
-                if (isset($app_settings->screen_view) && $app_settings->screen_view == '4') {
+                if (isset($app_settings->screen_view) && 
+                    $app_settings->screen_view == '4') {
                     echo 'selected';
                 }
                 ?>>Samsung Galaxy Y-2</option>
@@ -176,8 +208,11 @@ if ($view_list) {
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="form-hd">
-                            <form id="form-preview" action="" class="TTWForm" method="post" novalidate onSubmit="return submitform(this)">
-                                <input type="hidden" name="app_id" id="app_id" value="<?php echo $app_id; ?>" />
+                            <form id="form-preview" action="" 
+                                  class="TTWForm" method="post" 
+                                  novalidate onSubmit="return submitform(this)">
+                                <input type="hidden" name="app_id" id="app_id" 
+                                       value="<?php echo $app_id; ?>" />
                             </form>
                         </div>
                     </div>
@@ -196,21 +231,25 @@ if ($view_list) {
             <?php
             if ($this->acl->hasPermission('form', 'edit')) {
             if(count($forms)>1){?>
-            <li class="li-checked" style="padding: 20px!important;height: 22px;">Control Menu</li>
+            <li class="li-checked" style="padding: 20px!important;
+                                          height: 22px;">Control Menu</li>
             <?php }else{?>
-            <li id="edit_app_popup" style="cursor: pointer;height: 22px;">Edit Application</li>
+            <li id="edit_app_popup" style="cursor: pointer;
+                                           height: 22px;">Edit Application</li>
             <?php }
             }?>
             <?php
             foreach ($forms as $form) {
                 ?>
                 <li>
-                    <a href="<?= base_url() ?>app-form/<?php echo $form['form_id']; ?>" style="text-decoration:none;color:currentColor;">
+                    <a href="<?= base_url() ?>app-form/<?php echo $form['form_id'];
+                             ?>" style="text-decoration:none;color:currentColor;">
 
                     <?php
                     if (!empty($form['icon'])) {
                         ?>
-                        <img  class="formIconsUpload" alt="" title="<?php echo $form['title']; ?>" src="<?php echo FORM_IMG_DISPLAY_PATH . '../form_icons/' . $app_id . '/' . $form['icon'] ?>">
+                        <img  class="formIconsUpload" alt="" title="<?php echo $form['title']; ?>"
+                             src="<?php echo FORM_IMG_DISPLAY_PATH . '../form_icons/' . $app_id . '/' . $form['icon'] ?>">
                         <?php
                     } ?>
                         <span title="<?php echo $form['title'];?>">
